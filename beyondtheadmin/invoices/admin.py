45762726f7a06a69498299cc45b31828c1c92de1
Gregory Favre <greg@beyondthewall.ch>
2021-07-01 11:23:51 +0200

    fix - ordering of invoices

--- beyondtheadmin/invoices/admin.py
+++ beyondtheadmin/invoices/admin.py
@@ -9,7 +9,7 @@ class InvoiceLineInline(admin.TabularInline):
 
 @admin.register(Invoice)
 class InvoiceAdmin(admin.ModelAdmin):
-    readonly_fields = ['status_changed', 'total']
+    readonly_fields = ['status_changed', 'total', 'pdf', 'pdf_version']
     fields = (
         ('status', 'status_changed'),
         ('company', 'client'),
