e92412c81e5fce112b5e7f2eef09593846217e4b
Gregory Favre <greg@beyondthewall.ch>
2021-08-25 09:27:15 +0200

    fix - download link

--- beyondtheadmin/dashboard/context_processors.py
+++ beyondtheadmin/dashboard/context_processors.py
@@ -3,7 +3,7 @@ from beyondtheadmin.companies.models import Company
 
 
 def add_companies_to_context(request):
-    if not request.user.is_authenticated:
+    if not (hasattr(request, 'user') and request.user.is_authenticated):
         return {}
     return {
         'companies': Company.objects.filter(users=request.user)
