4e43625a1d4c01493b875bfc61d15018cee1190b
Gregory Favre <greg@beyondthewall.ch>
2022-05-19 10:49:01 +0200

    display clients in user admin

--- beyondtheadmin/users/admin.py
+++ beyondtheadmin/users/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 from beyondtheadmin.users.forms import UserChangeForm, UserCreationForm
 
 
 User = get_user_model()
+
+
+class ClientInline(admin.TabularInline):
+    model = User.clients.through
+    raw_id_fields = ('client',)
+    verbose_name = _("Clients")
+
+
+class CompanyInline(admin.TabularInline):
+    model = User.companies.through
+    raw_id_fields = ('company',)
 
 
 @admin.register(User)
@@ -17,10 +29,9 @@ class UserAdmin(auth_admin.UserAdmin):
         ("User", {
             "fields": ("name",)
         }),
-        ('Companies', {
-            'fields': ['companies']
-        })
+
     ) + tuple(auth_admin.UserAdmin.fieldsets)
 
     list_display = ["username", "name", "is_superuser"]
     search_fields = ["name"]
+    inlines = [ClientInline, CompanyInline]
