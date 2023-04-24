a95fffa8386fca96acf3e2ef6a50514036bac5dd
Gregory Favre <greg@beyondthewall.ch>
2023-04-24 16:27:03 +0200

    wip

---
+++ beyondtheadmin/companies/companies_app/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <CompanyForm />
+</template>
+
+<script>
+import CompanyForm from './components/CompanyForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CompanyForm
+  },
+  setup() {
+    const {t} = useI18n({
+      inheritLocale: true,
+      useScope: 'local'
+    })
+    return {t}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
