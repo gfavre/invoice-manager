8c8608337a4390a1bab90bb694e59e8d2d099e85
Gregory Favre <greg@beyondthewall.ch>
2023-05-23 11:31:07 +0200

    fix the edit title and subtitle

--- beyondtheadmin/companies/forms.py
+++ beyondtheadmin/companies/forms.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 from django import forms
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from colorfield.widgets import ColorWidget
 from crispy_forms.helper import FormHelper
@@ -78,98 +77,4 @@ class CompanyForm(forms.ModelForm):
                 css_class="border-left-primary shadow",
             ),
             ButtonHolder(Submit("submit", _("Save"), css_class="button white")),
-        )
-
-
-class CompanyCreateWizardBaseDataForm(forms.ModelForm):
-    class Meta:
-        model = Company
-        fields = [
-            "name",
-            "address",
-            "country",
-            "zip_code",
-            "city",
-            "phone",
-            "additional_phone",
-            "email",
-            "website",
-        ]
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_tag = False
-        self.helper.include_media = False
-        self.helper.layout = Layout(
-            Field("name", **{"v-model": "company.name"}),
-            Field("address", **{"v-model": "company.address"}),
-            Row(
-                Column(Field("country", **{"v-model": "company.country"}), css_class="col-md-4"),
-                Column(Field("zip_code", **{"v-model": "company.zip_code"}), css_class="col-md-2"),
-                Column(Field("city", **{"v-model": "company.city"})),
-            ),
-            "phone",
-            "additional_phone",
-            "email",
-            "website",
-        )
-
-
-class CompanyCreateWizardBankDataForm(forms.ModelForm):
-    class Meta:
-        model = Company
-        fields = ["vat_id", "iban", "name_for_bank", "bank", "bic"]
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_tag = False
-        self.helper.include_media = False
-        self.helper.layout = Layout(
-            Field("vat_id", **{"v-model": "company.vat_id"}),
-            Row(
-                Column(
-                    Field("iban", **{"v-model": "iban", ":class": "classIbanValid"}),
-                    css_class="col-md-6",
-                ),
-                Column("name_for_bank", css_class="col-md-6"),
-            ),
-            Row(
-                Column(Field("bank", v_model="bank", rows=3), css_class="col-9"),
-                Column(Field("bic", v_model="swift"), css_class="col"),
-            ),
-        )
-
-
-class CompanyCreateWizardConfigureInvoicesForm(forms.ModelForm):
-    class Meta:
-        model = Company
-        fields = [
-            "logo",
-            "contrast_color",
-            "invoice_note",
-            "signature_text",
-            "signature_image",
-            "email_signature",
-            "from_email",
-            "bcc_email",
-            "thanks",
-        ]
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_tag = False
-        self.helper.include_media = False
-        self.helper.layout = Layout(
-            "logo",
-            "contrast_color",
-            "invoice_note",
-            "signature_text",
-            "signature_image",
-            "email_signature",
-            Field("from_email", **{"v-model": "from_email"}),
-            "bcc_email",
-            "thanks",
         )
--- beyondtheadmin/companies/views/dashboard_views.py
+++ beyondtheadmin/companies/views/dashboard_views.py
@@ -4,55 +4,8 @@ from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, DetailView, TemplateView, UpdateView
 
-from ..forms import (
-    CompanyCreateWizardBankDataForm,
-    CompanyCreateWizardBaseDataForm,
-    CompanyCreateWizardConfigureInvoicesForm,
-    CompanyForm,
-)
+from ..forms import CompanyForm
 from ..models import Company
-
-
-class CompanyWizardView(LoginRequiredMixin, CreateView):
-    form_class = CompanyForm
-    success_url = reverse_lazy("dashboard")
-    template_name = "companies/wizard.html"
-    forms_classes = [
-        CompanyCreateWizardBaseDataForm,
-        CompanyCreateWizardBankDataForm,
-        CompanyCreateWizardConfigureInvoicesForm,
-    ]
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs["initial"].setdefault("from_email", self.request.user.email_from)
-        return kwargs
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        form_kwargs = self.get_form_kwargs()
-        context["form_base"] = CompanyCreateWizardBaseDataForm(**form_kwargs)
-        context["form_bank"] = CompanyCreateWizardBankDataForm(**form_kwargs)
-        context["form_invoices"] = CompanyCreateWizardConfigureInvoicesForm(**form_kwargs)
-        return context
-
-    def forms_valid(self, form):
-        forms_valid = True
-        for subform in form:
-            if not subform.is_valid():
-                forms_valid = False
-        if forms_valid:
-            self.object = form.save()
-            return HttpResponseRedirect(self.get_success_url())
-        else:
-            return self.form_invalid(form)
-
-    def form_valid(self, form):
-        """If the form is valid, save the associated model."""
-        # noinspection PyAttributeOutsideInit
-        self.object = form.save()
-        self.object.users.add(self.request.user)
-        return HttpResponseRedirect(self.get_success_url())
 
 
 class CompanyAppView(LoginRequiredMixin, TemplateView):
@@ -106,12 +59,7 @@ class CompanyDetailView(LoginRequiredMixin, DetailView):
     template_name = "companies/detail.html"
 
     def get_queryset(self):
-        return (
-            super(CompanyDetailView, self)
-            .get_queryset()
-            .filter(users=self.request.user)
-            .prefetch_related("invoices")
-        )
+        return super().get_queryset().filter(users=self.request.user).prefetch_related("invoices")
 
 
 class CompanyUpdateView(LoginRequiredMixin, UpdateView):
